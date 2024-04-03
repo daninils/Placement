0fc782b4d780733703845c214735185d813d7803
Daniel Nilsson <daniel.nilsson@geoguessr.com>
2024-04-03 12:01:33 +0200

    Anchor preference

--- Sources/DemoApp/DemoViews/CenterLayoutDemo.swift
+++ Sources/DemoApp/DemoViews/CenterLayoutDemo.swift
@@ -32,7 +32,7 @@ public struct CenterLayout: PlacementLayout {
         for index in subviews.indices {
             let subview = subviews[index]
             let dimension = subview.dimensions(in: proposal)
-            
+
             subview.place(
                 at: CGPoint(x: bounds.midX, y: bounds.midY),
                 anchor: .center,
@@ -44,6 +44,30 @@ public struct CenterLayout: PlacementLayout {
     public var prefersLayoutProtocol: Bool {
         nativeImplementation
     }
+}
+
+struct NativeAnchorPreference: PreferenceKey {
+  static var defaultValue: Anchor<CGRect>?
+
+  static func reduce(value: inout Anchor<CGRect>?, nextValue: () -> Anchor<CGRect>?) {
+    if let nextValue = nextValue() {
+      print("Native reduce new value")
+      value = nextValue
+    }
+  }
+}
+
+struct PlacementAnchorPreference: PreferenceKey {
+  static var defaultValue: Anchor<CGRect>?
+
+  static func reduce(value: inout Anchor<CGRect>?, nextValue: () -> Anchor<CGRect>?) {
+    // Using the following will use wrong position:
+//    value = nextValue()
+    if value == nil, let newValue = nextValue() {
+      print("Placement reduce new value")
+      value = newValue
+    }
+  }
 }
 
 struct CenterLayoutDemo: View {
@@ -58,7 +82,20 @@ struct CenterLayoutDemo: View {
                     Text("View 1")
                         .padding(changeLayout ? 10 : 20)
                         .border(.red)
+                        .anchorPreference(key: NativeAnchorPreference.self, value: .bounds) { $0 }
                 }
+                .overlayPreferenceValue(NativeAnchorPreference.self, { value in
+                  GeometryReader { proxy in
+                    if let value {
+                      let rect = proxy[value]
+                      let _ = print("Native rect: \(rect)")
+                      Rectangle()
+                        .fill(Color.green.opacity(0.5))
+                        .offset(x: rect.origin.x, y: rect.origin.y)
+                        .frame(width: rect.width, height: rect.height)
+                    }
+                  }
+                })
                 .border(.blue)
                 .padding()
                 .frame(width: 300, height: 200)
@@ -69,7 +106,21 @@ struct CenterLayoutDemo: View {
                     Text("View 1")
                         .padding(changeLayout ? 10 : 20)
                         .border(.red)
+                        .anchorPreference(key: PlacementAnchorPreference.self, value: .bounds) { $0 }
                 }
+                .overlayPreferenceValue(PlacementAnchorPreference.self, { value in
+                  GeometryReader { proxy in
+                    if let value {
+                      let rect = proxy[value]
+                      let _ = print("Placement rect: \(rect)")
+                      Rectangle()
+                        .fill(Color.green.opacity(0.5))
+                        .offset(x: rect.origin.x, y: rect.origin.y)
+                        .frame(width: rect.width, height: rect.height)
+                    }
+                    Text("Overlay")
+                  }
+                })
                 .border(.blue)
                 .padding()
                 .frame(width: 300, height: changeLayout ? 150 : 200)
